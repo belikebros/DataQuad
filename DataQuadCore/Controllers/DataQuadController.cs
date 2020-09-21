33652e61ccdeddcba731fab5f3c55b75a34d7bd0
Akshay H <aksh-h@users.noreply.github.com>
2020-09-21 11:24:33 +0530

    Add project files.

---
+++ DataQuadCore/Controllers/DataQuadController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WCFService;
+
+namespace DataQuadCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataQuadController : ControllerBase
+    {
+        private IDataQuadService _service;
+
+        public DataQuadController(IDataQuadService service)
+        {
+            _service = service;
+        }
+
+        // GET: api/dataquad/GetAllRaces
+        [HttpGet("GetAllRaces")]
+        public IActionResult GetAllRaces()
+        {
+            var ret = _service.GetAllRaces();
+            return Ok(ret);
+        }
+
+        // GET: api/dataquad/Sample
+        [HttpGet("Sample")]
+        public IActionResult Sample()
+        {
+            return Ok(_service.sample());
+        }
+    }
+}
